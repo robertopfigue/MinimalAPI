c9827c97b335005f4394051ed965bddda7cba424
Roberto Prado <robertopfigue@gmail.com>
2022-04-22 13:47:43 -0300

    initial

---
+++ DemoMinimalAPI/Data/MinimalContextDb.cs
@@ -0,0 +1,33 @@
+﻿using DemoMinimalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoMinimalAPI.Data
+{
+    public class MinimalContextDb : DbContext
+    {
+        public MinimalContextDb(DbContextOptions<MinimalContextDb> options) : base(options) { }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Fornecedor>()
+                .HasKey(e => e.Id);
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(e => e.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(e => e.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            modelBuilder.Entity<Fornecedor>()
+                .ToTable("Fornecedores");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
